4b50d57d512540b17b1a5f7a5b26ff0f5f25ec2b
shrimp-f <fujitadynamics@gmail.com>
2019-06-23 21:28:22 +0900

    画像の重ね合わせとエッジ検出(コード汚い)

--- edge.py
+++ edge.py
@@ -44,8 +44,49 @@ ORG_FILE_NAME = "org.jpg"
 GRAY_FILE_NAME = "gray.png"
 CANNY_FILE_NAME = "canny.png"
 
+
+width, height = im1.size
+a_2d = np.zeros((height,width,3))
+
+#すべての写真の画素を合計する
+for i in range(len(input_images)):
+    img_path = input_images[i]
+    print(img_path)
+    im_bottle = Image.open(str(img_path))
+    im_bottle = make_longer_than_width(im_bottle)
+
+    for y in range(height):
+        for x in range(width):
+#            print("y,x")
+#            print(y,x)
+#            a_2d[y,x] = imgdata[y * width + x]
+            r,g,b = im_bottle.getpixel((x,y))
+            a_2d[y,x][0] += r #R
+            a_2d[y,x][1] += g #G
+            a_2d[y,x][2] += b #B
+            '''
+            bg_2d[y,x][0] += r #R
+            bg_2d[y,x][1] += g #G
+            bg_2d[y,x][2] += b #B
+            '''
+
+#そしてすべての画素の平均をとる
+for y in range(height):
+    for x in range(width):
+        a_2d[y,x][0] = int(a_2d[y,x][0] / len(input_images)) #R
+        a_2d[y,x][1] = int(a_2d[y,x][1] / len(input_images))#G
+        a_2d[y,x][2] = int(a_2d[y,x][2] / len(input_images)) #B
+
+print(a_2d)
+#確か配列をpil形式に変換
+img = Image.fromarray(np.uint8(a_2d))
+img.show()
+
+#PIL形式をopenCV形式に変換
+img_cv=np.asarray(img)
+
 #閾値処理
-im2_cv = im1_cv
+im2_cv = img_cv
 min = np.array([98, 98, 98], np.uint8) #BGR #49,0,0だと緑消えた。0,49,0だと両方消えた。
 max = np.array([255, 255, 255], np.uint8)
 
@@ -56,12 +97,12 @@ plt.show()
 
 
 #org_img = cv2.imread(ORG_FILE_NAME, cv2.IMREAD_UNCHANGED)
-img = morph(blue_region)
+img_morph = morph(blue_region)
 # グレースケールに変換
 #gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 # エッジ抽出
 #canny_img = cv2.Canny(gray_img, 50, 110)
-canny_img = cv2.Canny(img, 50, 110)
+canny_img = cv2.Canny(img_morph, 50, 110)
 
 #plt.imshow(to_matplotlib_format(org_img))
 #plt.imshow(img)
